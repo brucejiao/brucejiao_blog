1a1a2809c74753a274665fd33eafa08f3ff96fc4
brucejiao <brucejiao@qq.com>
2018-12-14 16:43:59 +0800

    去除app.js无用信息

--- app.js
+++ app.js
@@ -19,8 +19,3 @@ free.on('exit', function (code, signal) {
     console.log('child process eixt ,exit:' + code);
 
 });
-
-作者：不系流年系乾坤
-链接：https://www.jianshu.com/p/a256ca175c64
-來源：简书
-简书著作权归作者所有，任何形式的转载都请联系作者获得授权并注明出处。
